0a5eaa6a3f3f33a700408cd70d55a7f0685240aa
chirila viorel <viorelchirila21@yahoo.ro>
2026-08-06 15:50:20 +0300

    Refactor: refactoring some code

--- include/CAN_DB.h
+++ include/CAN_DB.h
@@ -2,7 +2,7 @@
 #define CAN_DB_H
 
 /**
- * @file CAN_DB.h
+ * @file can_db.h
  * @author Chirila Viorel
  * @brief This file contains the definitions for the CAN message IDs and their corresponding payload structures.
  * @version 0.1
@@ -19,13 +19,13 @@
  */
 enum MESSAGE_ID
 {
-  WAKE_UP = 0x01,
-  SLEEP = 0x02,
-  NODE2_BCM_STATE = 0x03,
-  NODE1_DOOR_STATE = 0x04,
-  BUTTON_STATE = 0x100,
-  LED_STATE = 0x101,
-  DOOR_STATE = 0x111
+  CAN_MSG_WAKE_UP = 0x01,
+  CAN_MSG_SLEEP = 0x02,
+  CAN_MSG_NODE2_BCM_STATE = 0x03,
+  CAN_MSG_NODE1_DOOR_STATE = 0x04,
+  CAN_MSG_BUTTON_STATE = 0x100,
+  CAN_MSG_LED_STATE = 0x101,
+  CAN_MSG_DOOR_STATE = 0x111
 };
 
 /**
@@ -33,15 +33,15 @@ enum MESSAGE_ID
  */
 struct __attribute__((packed)) ButtonStateData
 {
-  uint8_t buttonDoorUpState : 1;
-  uint8_t buttonDoorDownState : 1;
-  uint8_t buttonInteriorLightState : 1;
+  uint8_t button_door_up : 1;
+  uint8_t button_door_down : 1;
+  uint8_t button_interior_light : 1;
   uint8_t reserved : 5;
 };
 
 union ButtonStatePayload
 {
-  ButtonStateData buttonState;
+  ButtonStateData data;
   uint8_t bytes[8];
 };
 
@@ -50,14 +50,14 @@ union ButtonStatePayload
  */
 struct __attribute__((packed)) LedStateData
 {
-  uint8_t ledDoorState : 1;
-  uint8_t ledInteriorLightState : 1;
+  uint8_t led_door : 1;
+  uint8_t led_interior_light : 1;
   uint8_t reserved : 6;
 };
 
 union LedStatePayload
 {
-  LedStateData ledState;
+  LedStateData data;
   uint8_t bytes[8];
 };
 
